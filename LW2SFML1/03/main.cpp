d8a99a1e41d9bd072bc2cf49d8e586e4f7d78f23
Nyaka <nyakatyan@bk.ru>
2017-09-23 19:23:38 +0300

    23.09.2017;19:23

--- LW2SFML1/03/main.cpp
+++ LW2SFML1/03/main.cpp
@@ -2,47 +2,31 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 
-#define marginRootShape 10  /*Отступ от края формы до края основы светофора*/
-#define paddingRootShape 10 /*Отступ от края основы светофора до его элементов*/
-#define radiusLight 100     /*Радиус элементов светофора*/
-
 int main()
 {
-    /* Основа светофора*/
-    sf::RectangleShape rootShape;
-    rootShape.setPosition({marginRootShape, marginRootShape});
-    rootShape.setSize({radiusLight * 2 + paddingRootShape * 2,
-                       radiusLight * 6 + paddingRootShape * 4});
-    rootShape.setFillColor(sf::Color(0x2F, 0x2F, 0x2F));
+    sf::ConvexShape triangle;
+    triangle.setFillColor(sf::Color(0xFF, 0x80, 0));
+    triangle.setPosition({200, 120});
+    triangle.setPointCount(3);
+    triangle.setPoint(0, {50, -20});
+    triangle.setPoint(1, {50, 20});
+    triangle.setPoint(2, {-100, 0});
 
-    /* Зелёный свет*/
-    sf::CircleShape lightGreen(radiusLight);
-    lightGreen.setPosition({rootShape.getPosition().x + paddingRootShape,
-                            rootShape.getPosition().y + paddingRootShape});
-    lightGreen.setFillColor(sf::Color(0x0, 0xFF, 0x0));
+    sf::ConvexShape trapeze;
+    trapeze.setFillColor(sf::Color(0xFF, 0, 0xFF));
+    trapeze.setPosition({400, 300});
+    trapeze.setPointCount(4);
+    trapeze.setPoint(0, {-90, 0});
+    trapeze.setPoint(1, {90, 0});
+    trapeze.setPoint(2, {120, 60});
+    trapeze.setPoint(3, {-120, 60});
 
-    /* Жёлтый свет*/
-    sf::CircleShape lightYellow(radiusLight);
-    lightYellow.setPosition({rootShape.getPosition().x + paddingRootShape,
-                             rootShape.getPosition().y + paddingRootShape * 2 + radiusLight * 2});
-    lightYellow.setFillColor(sf::Color(0xFF, 0xFF, 0x0));
-
-    /* Красный свет*/
-    sf::CircleShape lightRed(radiusLight);
-    lightRed.setPosition({rootShape.getPosition().x + paddingRootShape,
-                          rootShape.getPosition().y + paddingRootShape * 3 + radiusLight * 4});
-    lightRed.setFillColor(sf::Color(0xFF, 0x00, 0x0));
-
-    sf::RenderWindow window(sf::VideoMode({rootShape.getSize().x + marginRootShape * 2,
-                                           rootShape.getSize().y + marginRootShape * 2}),
-                            "TRAFFIC LIGHT");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Convex Shapes");
 
     window.clear();
 
-    window.draw(rootShape);
-    window.draw(lightGreen);
-    window.draw(lightYellow);
-    window.draw(lightRed);
+    window.draw(triangle);
+    window.draw(trapeze);
 
     window.display();
 
